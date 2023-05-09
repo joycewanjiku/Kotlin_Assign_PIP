d42c95cffa505dd1fb2c891a39c10c112e074e08
student <joycemumbiiwanjiku@gmail.com>
2023-05-09 23:21:40 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,42 @@
+fun main() {
+//    println(number.sum())
+//    println(number(.get[158]))
+//    println(number.sortedeByDesending())
+
+}
+
+//1. Create one function that is given the below array:
+//var numbers = arrayOf(32, 17,4,213,78,43,90,31,3,73,11,158,62)
+//(i) prints out the sum of the second and fifth elements (1 point)
+fun number(num:Int,num2:Int){
+    var numbers = arrayOf(32, 17,4,213,78,43,90,31,3,73,11,158,62)
+
+}
+
+//(ii) prints out the index of 158 (1 point)
+
+//(iii) prints out the elements in ascending order. (1 point)
+//2. Given a list of Person objects, each with the attributes, name,
+// age, height, and weight. Sort the list in order of descending age (2 points)
+fun people(name:String,age:Int,weight:Double){
+    var firsperson=("girl")
+    var secondperson=("boy")
+    var thirdperson=("man")
+
+}
+//
+//3. Create a function that takes in 3 names and returns a string array
+// containing all 3 names. (2 points)
+fun person(name1:String,name2:String,name3:String){
+    var name1="john"
+    var name2="Mumbi"
+    var name3="Kim"
+
+}
+
+//
+//4. Write a function that takes in a list of Car objects each with a registration and mileage
+// attribute and returns the average mileage of all the vehicles in the list. (3 points)
+fun car(regestration:Double,mileage:Double ){
+
+}
